1fd37859d3463c7d841cd7339e6d8a98c3c101a8
Josh Bristol <josh.bristol2@gmail.com>
2018-01-08 04:34:47 +0000

    Completed level 4

--- app.js
+++ app.js
@@ -18,22 +18,52 @@
 
 	});
 
-	app.controller('GalleryController', function() {
-		this.current = 0;
+	// app.controller('GalleryController', function() {
+	// 	this.current = 0;
 
-		this.setCurrent = function(newGallery) {
-			this.current = newGallery || 0;
-		};
+	// 	this.setCurrent = function(newGallery) {
+	// 		this.current = newGallery || 0;
+	// 	};
 
-	});
-	
-	app.controller("ReviewController", function(){
-		this.review={};
-		
-		this.addReview = function(product){
+	// });
+
+	app.controller("ReviewController", function() {
+		this.review = {};
+
+		this.addReview = function(product) {
 			this.review.createdOn = Date.now();
 			product.reviews.push(this.review);
-			this.review={};
+			this.review = {};
+		};
+	});
+
+	app.directive("productDescription", function() {
+		return {
+			restrict: 'E',
+			templateUrl: "product-description.html"
+
+		};
+	});
+
+	app.directive("productSpecs", function() {
+		return {
+			restrict: 'A',
+			templateUrl: "product-specs.html"
+		};
+	});
+
+	app.directive("productGallery", function() {
+		return {
+			restrict: 'E',
+			templateUrl: "product-gallery.html",
+			controller: function() {
+				this.current = 0;
+
+				this.setCurrent = function(newGallery) {
+					this.current = newGallery || 0;
+				};
+			},
+			controllerAs: "gallery",
 		};
 	});
 
@@ -42,6 +72,9 @@
 			price: 110.50,
 			description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
 			shine: 8,
+			rarity: 7,
+			color: '#CCC',
+			faces: 14,
 			canPurchase: true,
 			soldOut: false,
 			images: ["http://www.agta.org/gemstones/images/ruby.jpg", "http://www.mysticfamiliar.com/library/crystals/images/rbb.JPG", "http://ruby.org.in/wp-content/uploads/ruby-gemstone-3.jpg"],
@@ -62,6 +95,9 @@
 			price: 5.00,
 			description: "Origin of the Bloodstone is unknown, hence its low value. It has a very high shine and 12 sides, however.",
 			shine: 9,
+			rarity: 6,
+			color: '#EEE',
+			faces: 12,
 			canPurchase: true,
 			soldOut: false,
 			images: ["http://whataearth.com/wp-content/uploads/2013/12/Emerald_605.jpg", "http://www.vibgyorgems.com/images/subcategory/Medium/created_emerald_heart_small.jpg", "https://media.licdn.com/mpr/mpr/shrink_200_200/AAEAAQAAAAAAAApHAAAAJDkwZWY0MDc1LWE3YWQtNDUxNy04MDNkLTA5N2M2M2UxYzViZQ.png"],
@@ -82,6 +118,9 @@
 			price: 3.95,
 			description: "Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
 			shine: 70,
+			rarity: 2,
+			color: '#000',
+			faces: 6,
 			canPurchase: true,
 			soldOut: false,
 			images: ["https://www.gempundit.com/skin/frontend/ultimo/default/img/bg/amethyst-banner-1.png", "http://www.agta.org/gemstones/images/amethyst.jpg", "http://www.godsstones.com/images/GorgeousBrazilianAmethystGem.jpg"],
